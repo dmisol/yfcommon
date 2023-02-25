3773f19b22a0be85aa912324f0765e4c20355a0f
dmisol <dmi3sol@gmail.com>
2023-02-25 22:01:34 +0300

    env vars for URLs

--- pkg/model/env.go
+++ pkg/model/env.go
@@ -5,4 +5,7 @@ const (
 	EnvTgToken    = "tgtoken"
 	EnvRedisUsers = "users" // by hosts' email
 	EnvRedisTg    = "tginv" // tokens by random tg inv
+
+	EnvOauthUrl = "oauthurl" // oauth.key2open.site
+	EnvHost     = "hosturl"  // host.key2open.site
 )
