54081209f4e0e2e095e37caa0519adde21e93be4
dmisol <dmi3sol@gmail.com>
2024-11-05 00:46:38 +0300

    an option to notify issuer

--- pkg/model/guest.go
+++ pkg/model/guest.go
@@ -5,9 +5,9 @@ import "github.com/golang-jwt/jwt/v5"
 type GuestToken struct {
 	jwt.RegisteredClaims
 
-	Tz    string `json:"tz"`
-	Addr  string `json:"addr"`
-	DevId string `json:"devid,omitempty"`
-
-	Devices map[string]string `json:"devices,omitempty"` // by name, like "gate"
+	Tz        string            `json:"tz"`
+	Addr      string            `json:"addr"`
+	DevId     string            `json:"devid,omitempty"`
+	NotifyTid int64             `json:"notify,omitempty"`
+	Devices   map[string]string `json:"devices,omitempty"` // by name, like "gate"
 }
